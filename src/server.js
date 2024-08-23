6b37ca475d597b2c8b7a1ab6fc9c605b1c2f849d
im222b <rudgnsdleh@naver.com>
2024-08-23 14:28:00 +0900

    day6

--- src/server.js
+++ src/server.js
@@ -9,7 +9,9 @@ const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
+
 app.use("/public", express.static(__dirname + "/public"));
+
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
